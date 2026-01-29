a608c28e9b63b8739ed92cecc544be81289062bc
TifeTolex <olarogbatiolu@gmail.com>
2026-01-29 15:30:03 +0100

    fixed preorder price on home page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -8,7 +8,7 @@ const PRODUCTS = [
   { id: 4, title: 'Motive Sleeveless', price: 11000, priceOld : 13000, img: '/images/motive tank top.png', type: 'accessory'  , comingSoon: false },
   { id: 3, title: 'Motive Face Cap', price: 8500, priceOld: 10000, img: 'images/motive cap.png', type: 'accessory' , comingSoon: false },
   { id: 4, title: 'Motive Trucker Cap', price: 5000, priceOld: 6000,  img: '/images/motive trucker cap.png', type: 'accessory' , comingSoon: false },
-  { id: 4, title: 'Motive Joggers', price: 15000, priceOld: 15000, img: '/images/new3.png',type: 'accessory' , comingSoon: true },
+  { id: 4, title: 'Motive Joggers', price: 13500, priceOld: 15000, img: '/images/new3.png',type: 'accessory' , comingSoon: true },
 ];
 
 export default function Products() {
