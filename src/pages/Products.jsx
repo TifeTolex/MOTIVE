978016f002c6b6f60587d6d1926f38a9ff6c54fc
TifeTolex <olarogbatiolu@gmail.com>
2026-06-01 21:24:13 +0100

    reduced new series img sizes

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -9,9 +9,9 @@ const PRODUCTS = [
   { id: 3, title: 'Motive snapback', price: 10000,  img: 'images/motive cap.png', type: 'accessory' , comingSoon: false },
    { id: 3, title: 'Motive Face Cap', price: 8000,  img: 'images/motive face cap.png', type: 'accessory' , comingSoon: false },
   { id: 4, title: 'Motive Trucker Cap', price: 6000,   img: '/images/motive trucker cap.png', type: 'accessory' , comingSoon: false },
-    { id: 2, title: 'Motive Sleeve shirt', price: 12000, priceOld: 15000,  img: '/images/Black-for site.png',  type: 'accessory' , comingSoon: false },
-  { id: 4, title: 'Motive  Joggers', price: 13000, priceOld: 15000, img: '/images/black jogger.png', type: 'accessory'  , comingSoon: false },
-  { id: 4, title: 'Motive Beta Pair', price: 23000, priceOld: 27000,  img: '/images/black joggers and shirt for site.png',type: 'accessory' , comingSoon: false },
+   { id: 2, title: 'Motive Sleeve shirt', price: 12000,  img: '/images/mot 1.png',  type: 'accessory' , comingSoon: false },
+  { id: 4, title: 'Motive  Joggers', price: 13000,  img: '/images/mot 3.png', type: 'accessory'  , comingSoon: false },
+  { id: 4, title: 'Motive  Joggers', price: 13000,  img: '/images/mot 2.png', type: 'accessory'  , comingSoon: false },
 ];
 
 export default function Products() {
